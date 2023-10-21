2c970215662ffa1f1b47205902c9811acd43ce41
Joselito0015 <luis.blas@pucp.edu.pe>
2023-10-21 16:12:00 -0500

    +Websocket emit  everyone (interaction)

--- src/app.js
+++ src/app.js
@@ -7,4 +7,29 @@ const httpserver = app.listen(port, ()=>{
     console.log('listening on port ' + port)
 })
 
+const io = require('socket.io')(httpserver, {
+    cors: {
+        origin: '*',
+    }
+})
+
+
+io.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado');
   
+    socket.on('joinCompany', (companyId) => {
+      // El cliente se une a la sala de su compañía
+      socket.join(companyId);
+    });
+  
+    socket.on('interaction', ({ companyId, userData }) => {
+      // Verificar la autenticación y autorización aquí si es necesario
+  
+      // Emitir el evento solo a la sala de la compañía
+      socket.to(companyId).emit('interaction', userData);
+    });
+  
+    socket.on('disconnect', () => {
+      console.log('Cliente desconectado');
+    });
+  });
