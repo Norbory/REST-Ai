c81f635ac50ccb91f4f13053fa8232b8cdbca07f
Angelo M <a20162575@pucp.edu.pe>
2024-02-17 13:00:44 -0500

    Start sockets

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import { Server as WebSocketServer } from 'socket.io';
 
 const app = require('./index')
 const {port} = require('./config')
@@ -7,29 +8,10 @@ const httpserver = app.listen(port, ()=>{
     console.log('listening on port ' + port)
 })
 
-const io = require('socket.io')(httpserver, {
+const io = new WebSocketServer(httpserver, {
     cors: {
         origin: '*',
     }
-})
+});
 
-
-io.on('connection', (socket) => {
-    console.log('Nuevo cliente conectado');
-  
-    socket.on('joinCompany', (companyId) => {
-      // El cliente se une a la sala de su compañía
-      socket.join(companyId);
-    });
-  
-    socket.on('interaction', ({ companyId, userData }) => {
-      // Verificar la autenticación y autorización aquí si es necesario
-  
-      // Emitir el evento solo a la sala de la compañía
-      socket.to(companyId).emit('interaction', userData);
-    });
-  
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado');
-    });
-  });
+console.log('Socket.io server started')
