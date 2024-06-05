88d4bbf773806190dbb6c7697d996cbd83fd4ce0
Norbory <mandros.angelof@pucp.edu.pe>
2024-06-05 14:17:59 -0500

    Added cache and optimize

--- src/dao/class/dao.statistics.js
+++ src/dao/class/dao.statistics.js
@@ -1,13 +1,25 @@
 const { Company } = require('../models/company.model');
-
+let cache = {};
+let cacheUserCompany = {};
+let cacheUserCompanyEachMonth = {};
 class StatsDAO {
   // Get statistics by company
-  async getStatisticsByCompanyId (companyId) {
+  async getStatisticsByCompanyId(companyId) {
+    const cachedData = cache[companyId];
+    if (cachedData) {
+      // Every hour in milliseconds
+      const oneHour = 60 * 60 * 1000;
+      const now = Date.now();
+      if (now - cachedData.timestamp < oneHour) {
+        return cachedData.data;
+      }
+    }
     try {
       const company = await Company.findById(companyId);
       if (!company) {
         throw new Error('Compañía no encontrada');
       }
+
       let Cuentas = {
         Casco: 0,
         Chaleco: 0,
@@ -15,43 +27,39 @@ class StatsDAO {
         Lentes: 0,
         Orejeras: 0,
         Respirador: 0,
-        Total: 0
       };
+
       company.incidents.forEach(incident => {
         incident.EPPs.forEach(epp => {
-          switch (epp) {
-            case 'Casco':
-              Cuentas.Casco++;
-              break;
-            case 'Chaleco':
-              Cuentas.Chaleco++;
-              break;
-            case 'Guantes':
-              Cuentas.Guantes++;
-              break;
-            case 'Lentes':
-              Cuentas.Lentes++;
-              break;
-            case 'Orejeras':
-              Cuentas.Orejeras++;
-              break;
-            case 'Respirador':
-              Cuentas.Respirador++;
-              break;
-            default:
-              break;
+          if (Cuentas.hasOwnProperty(epp)) {
+            Cuentas[epp]++;
           }
         });
       });
-      Cuentas.Total = Cuentas.Casco + Cuentas.Chaleco + Cuentas.Guantes + Cuentas.Lentes + Cuentas.Orejeras + Cuentas.Respirador;
+
+      // Update the cache with the new data and the current timestamp
+      cache[companyId] = {
+        data: Cuentas,
+        timestamp: Date.now()
+      };
+
       return Cuentas;
     } catch (error) {
       throw error;
     }
-  };
+  }
 
   // Get statistics by user's company (for Supervisor)
-  async getStatisticsByUserCompany (companyId, userId) {
+  async getStatisticsByUserCompany(companyId, userId) {
+    const cachedData = cacheUserCompany[companyId];
+    if (cachedData) {
+      // Every hour in milliseconds
+      const oneHour = 60 * 60 * 1000;
+      const now = Date.now();
+      if (now - cachedData.timestamp < oneHour) {
+        return cachedData.data;
+      }
+    }
     try {
       const company = await Company.findById(companyId);
       if (!company) {
@@ -74,7 +82,7 @@ class StatsDAO {
         Respirador: 0,
         Total: 0
       };
-
+  
       const incidentesWithSupervisor = company.incidents.filter(incident => incident.supervisor === user.name);
       Cuentas.Total = incidentesWithSupervisor.length;
       const incidentesEliminados = incidentesWithSupervisor.filter(incident => incident.Deleted);
@@ -82,36 +90,19 @@ class StatsDAO {
       const incidentesAmonestados = incidentesEliminados.filter(incident => incident.Reported);
       Cuentas.Amonestados = incidentesAmonestados.length;
       Cuentas.Descartados = incidentesEliminados.length - incidentesAmonestados.length;
-
+  
       incidentesAmonestados.forEach(incident => {
-        if (incident.supervisor !== user.name) {
-          return Cuentas;
-        }
         incident.EPPs.forEach(epp => {
-          switch (epp) {
-            case 'Casco':
-              Cuentas.Casco++;
-              break;
-            case 'Chaleco':
-              Cuentas.Chaleco++;
-              break;
-            case 'Guantes':
-              Cuentas.Guantes++;
-              break;
-            case 'Lentes':
-              Cuentas.Lentes++;
-              break;
-            case 'Orejeras':
-              Cuentas.Orejeras++;
-              break;
-            case 'Respirador':
-              Cuentas.Respirador++;
-              break;
-            default:
-              break;
+          if (Cuentas.hasOwnProperty(epp)) {
+            Cuentas[epp]++;
           }
         });
       });
+      // Cache the result
+      cacheUserCompany[companyId] = {
+        data: Cuentas,
+        timestamp: Date.now()
+      };
       return Cuentas;
     } catch (error) {
       throw error;
@@ -120,6 +111,15 @@ class StatsDAO {
 
   // Get statistics by user's company sorted by month (for User)
   async getStatisticsByUserCompanyEachMonth (companyId, userId) {
+    const cachedData = cacheUserCompanyEachMonth[companyId];
+    if (cachedData) {
+      // Every hour in milliseconds
+      const oneHour = 60 * 60 * 1000;
+      const now = Date.now();
+      if (now - cachedData.timestamp < oneHour) {
+        return cachedData.data;
+      }
+    }
     try {
       const company = await Company.findById(companyId);
       if (!company) {
@@ -129,36 +129,25 @@ class StatsDAO {
       if (!user) {
         throw new Error('Usuario no encontrado');
       }
+  
       let Cuentas = {
         Nombre: user.name,
-        Mes:[{
-          nombre: '',
-          Registrados: 0,
-          Amonestados: 0,
-          Descartados: 0,
-          Casco: 0,
-          Chaleco: 0,
-          Guantes: 0,
-          Lentes: 0,
-          Orejeras: 0,
-          Respirador: 0,
-          Total: 0
-        }],
+        Mes: [],
       };
+  
       const incidentesWithSupervisor = company.incidents.filter(incident => incident.supervisor === user.name);
-      // Incidents current Year
       const incidentsByCurrentYear = incidentesWithSupervisor.filter(incident => incident.date.getFullYear() === new Date().getFullYear());
-      // Incidents by each month
+  
       const incidentsByMonth = incidentsByCurrentYear.reduce((acc, incident) => {
         const month = incident.date.getMonth();
         acc[month] = acc[month] || [];
         acc[month].push(incident);
         return acc;
       }, []);
-
+  
       Cuentas.Mes = incidentsByMonth.map((incidents, index) => {
         let CuentasMes = {
-          nombre: '',
+          nombre: new Date(2020, index).toLocaleString('es-ES', { month: 'long' }),
           Registrados: 0,
           Amonestados: 0,
           Descartados: 0,
@@ -170,52 +159,40 @@ class StatsDAO {
           Respirador: 0,
           Total: 0
         };
-        CuentasMes.nombre = new Date(2020, index).toLocaleString('es-ES', { month: 'long' });
-        // Cuentas por mes
-        const incidentesWithSupervisor = incidents.filter(incident => incident.supervisor === user.name);
-        CuentasMes.Total = incidentesWithSupervisor.length;
-        const incidentesEliminados = incidentesWithSupervisor.filter(incident => incident.Deleted);
-        CuentasMes.Registrados = CuentasMes.Total - incidentesEliminados.length;
-        const incidentesAmonestados = incidentesEliminados.filter(incident => incident.Reported);
-        CuentasMes.Amonestados = incidentesAmonestados.length;
-        CuentasMes.Descartados = incidentesEliminados.length - incidentesAmonestados.length;
-        // Cuentas por mes y por EPP
-        incidents.map((incident, index) => {
+  
+        incidents.forEach(incident => {
           if (incident.supervisor !== user.name) {
-            return CuentasMes;
+            return;
           }
+  
+          CuentasMes.Total++;
+          if (incident.Deleted) {
+            CuentasMes.Descartados++;
+            if (incident.Reported) {
+              CuentasMes.Amonestados++;
+            }
+          } else {
+            CuentasMes.Registrados++;
+          }
+  
           incident.EPPs.forEach(epp => {
-            switch (epp) {
-              case 'Casco':
-                CuentasMes.Casco++;
-                break;
-              case 'Chaleco':
-                CuentasMes.Chaleco++;
-                break;
-              case 'Guantes':
-                CuentasMes.Guantes++;
-                break;
-              case 'Lentes':
-                CuentasMes.Lentes++;
-                break;
-              case 'Orejeras':
-                CuentasMes.Orejeras++;
-                break;
-              case 'Respirador':
-                CuentasMes.Respirador++;
-                break;
-              default:
-                break;
+            if (CuentasMes.hasOwnProperty(epp)) {
+              CuentasMes[epp]++;
             }
           });
         });
+  
         return CuentasMes;
       });
-
+      // Cache the result
+      cacheUserCompanyEachMonth[companyId] = {
+        data: Cuentas,
+        timestamp: Date.now()
+      };
       return Cuentas;
-  } catch (error) {
-    throw error;
-  }
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
