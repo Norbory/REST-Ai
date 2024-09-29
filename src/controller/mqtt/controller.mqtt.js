7c334018dafbc201606d7374bc6e3c7aa4d58fbe
juanandres321 <a20185693@pucp.edu.pe>
2024-09-29 11:18:46 -0500

    DateFormatModified

--- src/controller/mqtt/controller.mqtt.js
+++ src/controller/mqtt/controller.mqtt.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const MqttDAO = require('../../dao/class/dao.mqtt');
 
 const Mqtt = new MqttDAO;
+const opcionesHora = { 
+    month: '2-digit', 
+    day: '2-digit' ,
+    year: 'numeric',
+    hour: '2-digit', 
+    minute: '2-digit',
+    second:'2-digit',
+    hour12: false };
 
 router.post('/:companyId/mensaje', async (req, res) => {
     const topic = req.body.topic;    
     const message = req.body.message; 
 
     try {
-        const response = await Mqtt.sendAndReceive(topic, JSON.stringify(message)); 
-        res.status(200).json({ topic, receivedMessage: response, dia: new Date().toLocaleDateString('es-Pe') }); 
+        const response = await Mqtt.sendAndReceive(topic, JSON.stringify(message));
+        const fecha = new Date(); 
+        res.status(200).json({ topic, receivedMessage: response, dia: new Intl.DateTimeFormat('es-ES', opcionesHora).format(fecha)}); 
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
